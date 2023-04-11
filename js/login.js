dfe3d621f5f654e917e769f4e32a6e14f6eef091
NicolasCeronM <nicolas134b@gmail.com>
2023-04-10 22:12:48 -0400

    new file:   css/dash-styl.css 	modified:   dashboard.html 	modified:   index.html 	new file:   js/login.js 	modified:   login.html

---
+++ js/login.js
@@ -0,0 +1,19 @@
+var form = document.getElementById('form');
+
+form.addEventListener('submit',login)
+
+
+function login(){  
+
+    let correo = document.getElementById('login-correo').value
+    let contraseña = document.getElementById('login-contra').value
+
+    if(correo === 'admin'){
+        alert('Bienvenido', correo);
+        location.href ="index.html"
+    }else{
+        alert('NOSE QUIEN ERI');
+    }
+
+    console.log(correo, contraseña);
+}
