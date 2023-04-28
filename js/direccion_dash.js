bbbc9f45279436837ea585d961fe0e5cb07f4ba1
NicolasCeronM <nicolas134b@gmail.com>
2023-04-28 18:33:11 -0400

    modified:   direcciones.html 	modified:   js/direccion_dash.js 	modified:   prueba.html 	modified:   registrar.html

--- js/direccion_dash.js
+++ js/direccion_dash.js
@@ -1,16 +1,29 @@
+class Direccion {
+  constructor(direc, numero, region, comuna, cod_postal) {
+    this.direc = direc;
+    this.numero = numero;
+    this.region = region;
+    this.comuna = comuna;
+    this.cod_postal = cod_postal;
+  }
+}
+
 var userLogiado = JSON.parse(localStorage.getItem("userLogiado"));
+var users = JSON.parse(localStorage.getItem("localUserList"));
+
 const userDirec = userLogiado.direc;
+const ref = document.getElementById("refrescar");
+
 //Ingresar direcciones
 const curepoTable = document.getElementById("cuerpo-tabla");
 htmlTable = "";
 
+const tabla = document.getElementById("cuerpo-tabla");
+
 //Funcion para cargra objeto a la tabla
 function cargarTabla() {
-  const tabla = document.getElementById("cuerpo-tabla");
-
   userDirec.forEach((datos) => {
     const fila = document.createElement("tr");
-
     const direcTd = document.createElement("td");
     direcTd.textContent = datos.direc;
     fila.appendChild(direcTd);
@@ -32,20 +45,23 @@ function cargarTabla() {
     fila.appendChild(codPostalTd);
 
     //Boton editas
+    const btnEditar = document.createElement("button");
 
     const accionesTd = document.createElement("td");
-    const btnEditar = document.createElement("button");
-    const iconEditar = document.createElement('i');
+    const iconEditar = document.createElement("i");
     iconEditar.classList.add("fa-regular", "fa-pen-to-square");
     btnEditar.appendChild(iconEditar);
-    btnEditar.classList.add("btn","btn-warning","me-3");
+    btnEditar.classList.add("btn", "btn-warning", "me-3");
     accionesTd.appendChild(btnEditar);
 
     //Boton borrar
     const btnBorrar = document.createElement("button");
-    const iconBorrar = document.createElement('i');
+    btnBorrar.addEventListener('click',()=>{
+        fila.remove();
+    })
+
+    const iconBorrar = document.createElement("i");
     iconBorrar.classList.add("fa-solid", "fa-trash-can");
-    iconBorrar.style.color = 'black';
     btnBorrar.appendChild(iconBorrar);
     btnBorrar.classList.add("btn", "btn-danger");
     accionesTd.appendChild(btnBorrar);
@@ -55,7 +71,80 @@ function cargarTabla() {
     tabla.appendChild(fila);
   });
 }
-
 curepoTable.innerHTML = htmlTable;
 
+const btnRegistrar = document.getElementById("regNuevaDirec");
+
+//Funcion Obtener datos de registrar
+function nuevaDireccion() {
+  const direc = document.getElementById("nuevaDirec");
+  const num = document.getElementById("nuevoNum");
+  const region = document.getElementById("regRegion");
+  const comuna = document.getElementById("regComuna");
+  const cpostal = document.getElementById("nuevoCpo");
+
+  var nuevaDireccion2 = new Direccion(
+    direc.value,
+    num.value,
+    region.value,
+    comuna.value,
+    cpostal.value
+  );
+  userLogiado.direc.push(nuevaDireccion2);
+  actualizarTabla();
+}
+
+//Funcion registrar nueva direccion
+function actualizarTabla() {
+    // Obtenemos la tabla
+    const tabla = document.getElementById('tabla-direc');
+
+    // Borramos todas las filas de la tabla
+    tabla.querySelectorAll('tbody tr').forEach(tr => tr.remove());
+
+    // Agregamos las filas con los nuevos datos
+    userDirec.forEach(dato => {
+        
+      // Creamos una nueva fila y llenamos las celdas con los datos del objeto
+      const fila = document.createElement('tr');
+      fila.innerHTML = `
+        <td>${dato.direc}</td>
+        <td>${dato.numero}</td>
+        <td>${dato.region}</td>
+        <td>${dato.comuna}</td>
+        <td>${dato.cod_postal}</td>
+        <td>
+          <button class="btn btn-warning"><i class="fas fa-edit"></i></button>
+          <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+        </td>
+      `;
+      // Agregamos la fila a la tabla
+      tabla.querySelector('tbody').appendChild(fila);
+    });
+
+    //console.log(userLogiado);
+    agregarLocalStorage(users);
+  }
+
+  function agregarLocalStorage(user){
+
+    for (let i in user){
+        if(user[i].rut === userLogiado.rut){
+
+            //console.log(userLogiado)
+            user.splice(i)
+            user.push(userLogiado);
+            break;
+        }
+    }
+    localStorage.setItem("localUserList", JSON.stringify(user));
+  }
+
+
+/*
+--------------------------------------------------------
+                Escuchar eventos
+---------------------------------------------------------
+*/
+btnRegistrar.addEventListener("click", nuevaDireccion);
 addEventListener("DOMContentLoaded", cargarTabla);
