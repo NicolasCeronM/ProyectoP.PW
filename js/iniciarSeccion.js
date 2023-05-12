29a059c6b1b316dd162f032bc908d884480aabb3
NicolasCeronM <nicolas134b@gmail.com>
2023-05-12 18:33:02 -0400

    modified:   js/iniciarSeccion.js 	modified:   login.html

--- js/iniciarSeccion.js
+++ js/iniciarSeccion.js
@@ -8,28 +8,57 @@ const bIniciar = document.getElementById("login-enviar");
 let estaLogeado = false;
 let usarioLogiado = "";
 
+var Campos ={
+  mail: false,
+  pass:false
+}
+
 bIniciar.addEventListener("click", login);
 
-var ver = document.getElementById('ver-contraseña');
-var verIcon = document.getElementById('ver-icon');
-function Mostrar(e){
-  e.preventDefault();
+var ver = document.getElementById("ver-contraseña");
+var verIcon = document.getElementById("ver-icon");
 
-  if(pass.type === 'password'){
-
-    pass.type = 'text'
-    verIcon.classList.remove('fa-regular','fa-eye');
-    verIcon.classList.add('fa-regular', 'fa-eye-slash');
-    
-    
-
-  }else{
-    pass.type = 'password';
-    verIcon.classList.remove('fa-regular', 'fa-eye-slash');
-    verIcon.classList.add('fa-regular','fa-eye');
+function ValidarCorreo() {
+  var expR =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  //CAMPO CORREO
+  if (expR.test(mail.value)) {
+    mail.classList.remove("is-invalid");
+    mail.classList.add("is-valid");
+    campos.mail = true;
+  } else {
+    mail.classList.add("is-invalid");
+    campos.mail = false;
   }
 }
-ver.addEventListener('click',Mostrar);
+function ValidarPass() {
+  //CAMPO CONTRASEÑA
+  if (pass.value.length <= 12 && pass.value.length >= 5) {
+    pass.classList.remove("is-invalid");
+    pass.classList.add("is-valid");
+    campos.pass = true;
+  } else {
+    pass.classList.add("is-invalid");
+    campos.pass = false;
+  }
+}
+
+function Mostrar(e) {
+  e.preventDefault();
+
+  if (pass.type === "password") {
+    pass.type = "text";
+    verIcon.classList.remove("fa-regular", "fa-eye");
+    verIcon.classList.add("fa-regular", "fa-eye-slash");
+  } else {
+    pass.type = "password";
+    verIcon.classList.remove("fa-regular", "fa-eye-slash");
+    verIcon.classList.add("fa-regular", "fa-eye");
+  }
+}
+ver.addEventListener("click", Mostrar);
+mail.addEventListener("keyup", ValidarCorreo);
+pass.addEventListener("keyup", ValidarPass);
 
 function login() {
   for (let i = 0; i < userList.length; i++) {
@@ -63,5 +92,5 @@ function login() {
 
 function agregarLocalStorage(usuario) {
   localStorage.setItem("userLogiado", JSON.stringify(usuario));
-  localStorage.setItem('showAlert', JSON.stringify(false));
+  localStorage.setItem("showAlert", JSON.stringify(false));
 }
